f2772c16ff8e23e59ed4131fdf7d12908e31b37a
PrativaG <c7.paru@gamil.com>
2020-03-31 21:24:30 -0700

    ignore my changes

--- client/src/components/games/TypeFasterMaster.js
+++ client/src/components/games/TypeFasterMaster.js
@@ -4,6 +4,12 @@ import { connect } from 'react-redux';
 
 import styles from './Games.module.css';
 
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+
 import NavBar from '../NavBar';
 
 var randomWords = require('random-words');
@@ -132,7 +138,11 @@ const TypeFasterMaster = ({  socket, userName, roomName, userScore })  => {
     return (
         <>
         <NavBar />
-        <div className={styles.entirePage}>
+        <div style={{'backgroundColor' : 'white'}} className={styles.entirePage}>
+            <FormControl variant="outlined">
+                <InputLabel htmlFor="component-outlined">Name</InputLabel>
+                <OutlinedInput id="component-outlined" label="Name" />
+            </FormControl>
              <p className={styles.textWhite}>{resultMsg}</p>
             <h3 className={styles.textWhite}> <i> {message} {userName}</i>  </h3>
            {
