cbc960a16b38b0015cca9249dc5960d628b3bd37
PrativaG <c7.paru@gamil.com>
2020-03-31 20:20:50 -0700

    TypeFaster works now!

--- client/src/views/Lobby.js
+++ client/src/views/Lobby.js
@@ -1,11 +1,11 @@
 // [MAIN COMPONENTS]
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { navigate } from '@reach/router';
 import { connect } from 'react-redux';
 
 // [STYLES]
 import styles from './Views.module.css';
-
+// import {NavPills} from 'material-kit-react';
 // [NAVBAR]
 // import NavBar from '../components/NavBar';
 
@@ -19,13 +19,14 @@ import styles from './Views.module.css';
 // import DropAFatShot from '../components/games/DropAFatShot';
 
 const Lobby = ({ socket, dispatch, userName }) => {
+
     const [ formState, setFormState ] = useState({
         uName: "",
         rName: "",
         rPassword: ""
     });
     const [ errorMsg, setErrorMsg ] = useState ("");
-
+   
     const onChangeHandler = e => {
         setFormState({
             ...formState,
@@ -46,6 +47,16 @@ const Lobby = ({ socket, dispatch, userName }) => {
             //     roomName: formState.rName,
             // },
             );
+            
+            //[Giving ROOMNAME to SOCKETS, .on required because socket will not have room name otherwise]
+            // socket.on('connect', ()=>{
+            //     console.log("Inside Lobby.. Going to create room!");
+            //     console.log(formState.rName);
+            //     socket.emit('room created', {
+            //         roomName : formState.rName
+            //     });
+            // })
+             
             navigate('/'+formState.rName);
         } else {
             setErrorMsg("Please enter both a user and room name");
@@ -58,6 +69,7 @@ const Lobby = ({ socket, dispatch, userName }) => {
         <>
         <div className={[styles.flexColCen, styles.textWhite, styles.onlyDiv].join(' ')}>
             <br/>
+
             <h1>Mini Game Party!</h1>
                 <br/>
             <p className={styles.textRed}>Please enter your desired nick name and room you wish to enter or create</p> 
