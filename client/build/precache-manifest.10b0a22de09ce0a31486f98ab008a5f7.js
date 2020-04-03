d73cd4803461c8cbc0a04409bb19477a577b6a6e
albertfromsd <58611720+albertfromsd@users.noreply.github.com>
2020-04-03 13:02:41 -0700

    After run build

---
+++ client/build/precache-manifest.10b0a22de09ce0a31486f98ab008a5f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121445731415b98570d164eb745c5abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c52e9938338a614c61b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "60debe79b0c537999ada",
+    "url": "/static/css/main.5e384853.chunk.css"
+  },
+  {
+    "revision": "7c52e9938338a614c61b",
+    "url": "/static/js/2.d0b857c0.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.d0b857c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60debe79b0c537999ada",
+    "url": "/static/js/main.d47eecd9.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "87e5da07a2316cfc5608ede891de0375",
+    "url": "/static/media/game (1).87e5da07.gif"
+  },
+  {
+    "revision": "50a2c1df928fd5ab602ba875a4178422",
+    "url": "/static/media/game (2).50a2c1df.gif"
+  },
+  {
+    "revision": "7682298ce34d109f6da992f8e5c2f940",
+    "url": "/static/media/game (3).7682298c.gif"
+  }
+]);
